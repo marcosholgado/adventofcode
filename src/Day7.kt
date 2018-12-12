4081e1e67c0562968e98dc338aa40d3ad0facca9
Marcos Holgado <marcos.holgadomolina@sky.uk>
2018-12-12 19:28:47 +0000

    Solve day 7 part 2

--- src/Day7.kt
+++ src/Day7.kt
@@ -1,9 +1,10 @@
+
 import java.io.File
 import kotlin.collections.HashMap
 
 fun main(args: Array<String>) {
     println(Day7.part1(File("src/day7.txt").readLines()))
-    //println(Day7.part2(File("src/day7.txt").readLines(), 10000))
+    println(Day7.part2(File("src/day7.txt").readLines(), 5, 60))
 }
 
 object Day7 {
@@ -22,7 +23,6 @@ object Day7 {
             if (!map.containsKey(child)) {
                 map[child] = TreeNode(child)
             }
-
             map[value]?.addChild(map[child]!!)
         }
 
@@ -56,9 +56,80 @@ object Day7 {
         return result
     }
 
+    fun part2(steps: List<String>, numWorkers: Int, dur: Int): String {
+        var result = ""
+        var seconds = -1
+        val map = HashMap<String, TreeNode<String>>()
+
+        steps.map {
+            val splits = it.split(" ")
+            val value = splits[1]
+            val child = splits[7]
+            if (!map.containsKey(value)) {
+                map[value] = TreeNode(value)
+            }
+            if (!map.containsKey(child)) {
+                map[child] = TreeNode(child)
+            }
+            map[value]?.addChild(map[child]!!)
+        }
+
+        val rootList = mutableListOf<String>()
+        val availableSteps = sortedSetOf<String>()
+        val workers = mutableListOf<Worker>()
+
+        map.map {
+            val node = it.value
+            if(node.parent.size == 0) rootList.add(node.value)
+        }
+        rootList.sort()
+
+        availableSteps.addAll(rootList)
+
+        for(i in 0 until numWorkers) {
+            workers.add(Worker(0, null))
+        }
+
+        var workerAvailable: Int
+        while(result.length < map.size) {
+            seconds++
+
+            workers.map {
+                it.seconds--
+                if (it.seconds == 0) {
+                    result += it.node!!.value
+                    val node = map[it.node!!.value]!!
+                    node.finished = true
+                    it.node?.children?.map { child ->
+                        var add = true
+                        child.parent.map { parent ->
+                            add = add && parent.used && parent.finished
+                        }
+                        if (add && !child.used) availableSteps.add(child.value)
+                    }
+                }
+            }
+
+            workerAvailable = workers.indexOfFirst { it.seconds <= 0 }
+            while(workerAvailable != -1 && availableSteps.size > 0) {
+                val step = availableSteps.first()
+                val node = map[step]!!
+                workers[workerAvailable].seconds = step.single().toInt() + dur - 64
+                workers[workerAvailable].node = node
+                node.used = true
+                availableSteps.remove(step)
+                workerAvailable = workers.indexOfFirst { it.seconds <= 0 }
+            }
+        }
+        return seconds.toString()
+    }
+
+    data class Worker(var seconds: Int, var node: TreeNode<String>?)
+
     class TreeNode<T>(var value: T){
         var parent:MutableList<TreeNode<T>> = mutableListOf()
         var used = false
+        var finished = false
         var children:MutableList<TreeNode<T>> = mutableListOf()
 
         fun addChild(node:TreeNode<T>){
--- test/Day7Test.kt
+++ test/Day7Test.kt
@@ -19,4 +19,19 @@ internal class Day7Test {
         val out = Day7.part1(input)
         assertEquals("CABDFE", out)
     }
+
+    @Test
+    fun testDay72() {
+        val input = listOf(
+            "Step C must be finished before step A can begin.",
+            "Step C must be finished before step F can begin.",
+            "Step A must be finished before step B can begin.",
+            "Step A must be finished before step D can begin.",
+            "Step B must be finished before step E can begin.",
+            "Step D must be finished before step E can begin.",
+            "Step F must be finished before step E can begin."
+        )
+        val out = Day7.part2(input, 2, 0)
+        assertEquals("15", out)
+    }
 }
